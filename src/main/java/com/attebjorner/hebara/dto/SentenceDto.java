304072dc4e30442ea117f3d6bf6eab5c6a68f25b
gosha kalosha <goshakalosha1337@gmail.com>
2021-06-05 19:47:12 +0300

    auth

--- src/main/java/com/attebjorner/hebara/dto/SentenceDto.java
+++ src/main/java/com/attebjorner/hebara/dto/SentenceDto.java
@@ -1,31 +1,15 @@
 package com.attebjorner.hebara.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.util.List;
-
 public class SentenceDto
 {
     private long id;
 
     private String originalSentence;
 
-//    TODO
-//    private String translation;
-
-//    @JsonProperty("words_positions")
-//    private List<Integer> requestedWordsPositions;
-//
-//    @JsonProperty("words_ids")
-//    private List<Long> wordsIds;
-
     public SentenceDto(long id, String originalSentence)
     {
         this.id = id;
         this.originalSentence = originalSentence;
-//        this.translation = translation;
-//        this.requestedWordsPositions = requestedWordsPositions;
-//        this.wordsIds = wordsIds;
     }
 
     public long getId()
@@ -37,19 +21,4 @@ public class SentenceDto
     {
         return originalSentence;
     }
-
-//    public String getTranslation()
-//    {
-//        return translation;
-//    }
-//
-//    public List<Integer> getRequestedWordsPositions()
-//    {
-//        return requestedWordsPositions;
-//    }
-//
-//    public List<Long> getWordsIds()
-//    {
-//        return wordsIds;
-//    }
 }
--- src/main/java/com/attebjorner/hebara/exception_handling/NothingFound.java
+++
@@ -1,5 +0,0 @@
-package com.attebjorner.hebara.exception_handling;
-
-public class NothingFound
-{
-}
